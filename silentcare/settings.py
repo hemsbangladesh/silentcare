f173a94f9d70e89ecc529973348977df89dcda8e
Tanzil <tanzilo@gmail.com>
2026-03-20 22:23:26 +0600

    2026-20-03 04:23:18 PM

--- silentcare/settings.py
+++ silentcare/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["silentcare.com",
-                 "silent-care-m4pnd.ondigitalocean.app", "127.0.0.1"]
+ALLOWED_HOSTS = ["silentcare.org", "silent-care-m4pnd.ondigitalocean.app", "127.0.0.1"]
 
 # Application definition
 
