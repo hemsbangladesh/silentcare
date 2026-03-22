e941faab3b12ceaa632c69083c9119c2f0bd30c9
Tanzil <tanzilo@gmail.com>
2026-03-22 12:00:57 +0600

    2026-22-03 06:00:45 AM

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = []
+from django.urls import path
+from .views import *
+
+app_name = "core"  # optional but recommended
+# <a href="{% url 'core:home' %}">Back to Home</a>
+# <a href="{% url 'core:post_detail' pk=5 %}">Open Post 5</a>
+
+urlpatterns = [
+    path('core/categories', CoreCategoriesPageView.as_view(), name='core-categories'),
+]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,26 @@
-# from django.views.generic import TemplateView
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from core.models import *
 
-# class HomePageView(TemplateView):
-# 	template_name = 'home.html'
+class CoreCategoriesPageView(TemplateView):
+	template_name = 'output.html'
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['title'] = "Categories &amp; sub-categories"
+		context['headline'] = "Categories &amp; sub-categories"
+		context['output'] = ""
+		return context
+
+	def get(self, request, *args, **kwargs):
+		context = self.get_context_data()
+		output = context['output']
+		counter = 0
+		active_categories = Categories.objects.filter(is_active="Y").order_by("name")
+		for active_category in active_categories:
+			counter = counter + 1
+			category = str(active_category.name).strip()
+			output = output + '<p>{}. {}</p>'.format(counter, category)
+
+		context['output'] = output
+		return render(request, self.template_name, context)
